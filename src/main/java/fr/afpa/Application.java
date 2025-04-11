3c7180fba00a427df2948de38fe6bf65e41e6985
konchu974 <156214774+konchu974@users.noreply.github.com>
2025-04-11 08:23:46 +0200

    ajout des exercices 11/04/25

---
+++ src/main/java/fr/afpa/Application.java
@@ -0,0 +1,521 @@
+package fr.afpa;
+
+import java.awt.*;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * The type Application.
+ */
+public class Application {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args)
+    {
+
+//-------------- Exercice 3.4------------------------------------------
+
+//        enoughMoney();
+
+//-------------- Exercice 3.5------------------------------------------
+//        lunchPrice();
+
+//-------------- Exercice 5.7------------------------------------------
+//        manipTab();
+
+        //-------------- Exercice 8------------------------------------------
+        plusOuMoins();
+
+    }
+
+
+    /**
+     * Is adult.
+     *
+     * @param birthYear the birth year
+     * @return
+     */
+    public static String isAdult(int birthYear){
+        if (birthYear > 2007){
+            System.out.println("Vous êtes mineur");
+        }else{
+            System.out.println("Vous êtes majeur");
+        }
+        return "";
+    }
+
+    /**
+     * Find max min.
+     *
+     * @param var1 the var 1
+     * @param var2 the var 2
+     * @param var3 the var 3
+     */
+    public static void findMaxMin(int var1, int var2, int var3){
+//        int min = var1;
+//        int max = var1;
+//
+//        if (var2 < min) {
+//            min = var2;
+//        }
+//        if (var3 < min) {
+//            min = var3;
+//        }
+//
+//        if (var2 > max) {
+//            max = var2;
+//        }
+//        if (var3 > max) {
+//            max = var3;
+//        }
+//
+//        System.out.println("Le maximum est : " + max + " et le minimum est : " + min);
+
+        int min = Integer.min(var1, var2);
+        int max = Integer.max(var2, var3);
+        System.out.println(min);
+        System.out.println(max);
+
+
+
+    }
+
+    /**
+     * Voyelle ou consonne.
+     *
+     * @param letter the letter
+     */
+    public static void voyelleOuConsonne(char letter){
+//        a, e, i, o, u, y
+
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'y'){
+            System.out.println("v");
+        }else{
+            System.out.println("c");
+
+        }
+    }
+
+    /**
+     * Enough money.
+     */
+    public static void enoughMoney(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entrez votre solde de compte: ");
+
+       int  soldeCompte = scanner.nextInt();
+       if (soldeCompte<0){
+           System.out.println("Vous ne pouvez pas être a découvert");
+       }else {
+
+       System.out.println("Entrez le prix de votre produit: ");
+
+       int productPrice = scanner.nextInt();
+
+
+           if (soldeCompte < productPrice) {
+               System.out.println("Vous ne pouvez pas acheter le produit.");
+           } else {
+               System.out.println("Vous pouvez acheter le produit et il vous restera: " + (soldeCompte - productPrice));
+           }
+       }
+    }
+
+    /**
+     * Lunch price.
+     */
+    public static void lunchPrice(){
+        boolean restartCal = true;
+
+        while ( restartCal){
+            int montParticipation = 0;
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Quel est votre état matrimonial ('C' pour celibataire, 'A' pour autre): ");
+            String etatMatri  = scanner.next( );
+
+            System.out.println("Nombre d'enfants à charge: ");
+            int nbEnfant  = scanner.nextInt( );
+
+            System.out.println("Quel est votre salaire mensuel: ");
+            int salaireMens  = scanner.nextInt( );
+
+            if (etatMatri == "C" || etatMatri == "c"){
+                montParticipation = 20;
+
+            }else {
+                montParticipation = 25;
+
+            }
+
+            montParticipation += (15 * nbEnfant);
+
+
+            if (salaireMens < 1800){
+                montParticipation += 10;
+            }
+
+            if (montParticipation>50){
+                montParticipation =50;
+
+            }
+            System.out.println("le taux de participation est de: " + montParticipation + " Souhaitez-vous effectuer le calcul pour un autre employé ? (si oui 'O', si non 'N')");
+
+            String reponseValid  = scanner.next( );
+
+            if (reponseValid.equalsIgnoreCase("N")){
+                restartCal = false;
+            }
+        }
+
+    }
+
+    /**
+     * Effectue la somme de 2 entiers
+     *
+     * @param a premier entier
+     * @param b deuxième entier
+     * @return résultat de la somme
+     */
+
+    public static int subYoutubeFor(int subscriber, int mois, double coeff){
+
+        for (int i = 1; i <= mois; i++){
+            subscriber += subscriber * coeff;
+            System.out.println("vous avez " + subscriber + " sur le mois " + i);
+        }
+        System.out.println("dans " + mois + " mois vous aurez " + subscriber);
+        return subscriber;
+    }
+
+    public static int subYoutubeWhile(int subscriber, int mois, double coeff){
+        int i = 1;
+        while (i<= mois){
+            subscriber += subscriber * coeff;
+            System.out.println("vous avez " + subscriber + " sur le mois " + i);
+            i ++;
+        }
+        System.out.println("dans " + mois + " mois vous aurez " + subscriber);
+        return subscriber;
+
+    }
+
+    public static int sommeEntier ( int n){
+        int entier = 0;
+
+        for (int i = 0; i <= n; i++) {
+            entier += i;
+            System.out.println(entier);
+        }
+
+        return entier;
+    }
+
+    public static void fizzBuzz (int n){
+        for (int i = 0; i <= n; i++){
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+
+        }
+    }
+
+    public static int calcFacto (int n){
+        int ndiff = 1;
+        for (int i = 1; i <= n; i++) {
+            ndiff *= i;
+        }
+        return ndiff;
+    }
+
+    public static void afficherTab (){
+        double[] doubleArray = {12.2, 11.3, 4.4, 2.0};
+
+        for (int i = 0; i < doubleArray.length; i++) {
+            System.out.println(doubleArray[i]);
+        }
+    }
+
+    public static int rechercheSeqFor(int value){
+        int findValue = 0;
+        int[] intArray = {5, 6, 3, 12, 25};
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] == value){
+                findValue = intArray[i] ;
+            }
+        }
+        return findValue;
+    }
+
+    public static int rechercheSeqWhile(int value){
+        int n =0;
+        int[] intArray = {5, 6, 3, 12, 25};
+        while (intArray[n] != value){
+            n++;
+        }
+        return intArray[n];
+    }
+
+    public static int rechercheSeqDoWhile(int value){
+        int i = 0;
+        int[] intArray = {5, 6, 3, 12, 25};
+
+        do {
+            i++;
+        }while (intArray[i] != value);
+
+        return intArray[i];
+    }
+
+    public static int sumBetween(int firstVal, int endVal){
+        int resultSum = 0;
+        int[] sumInTab = {5, 12, 6, 1, 7, 8, 3};
+
+        while (firstVal<=endVal){
+            resultSum += sumInTab[firstVal];
+            firstVal++;
+        }
+        return resultSum;
+    }
+
+    public static int[] minMaxTab(int[] tabEntree){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < tabEntree.length; i++) {
+            if(tabEntree[i]<min){
+                min = tabEntree[i];
+            }
+            if(tabEntree[i]>max){
+                max = tabEntree[i];
+            }
+        }
+        int[] tabMinMax = {min, max};
+    return tabMinMax;
+    }
+
+    public static boolean swap(int firstVal, int secondVal){
+        boolean validation = false;
+        int[] swapTab = {4, 6, 2, 155};
+
+        if (firstVal<= swapTab.length && secondVal<=swapTab.length){
+            int swap1 = swapTab[firstVal];
+            int swap2 = swapTab[secondVal];
+
+            swapTab[firstVal] = swap2;
+            swapTab[secondVal] = swap1;
+
+            for (int i = 0; i < swapTab.length; i++) {
+                System.out.println(swapTab[i]);
+            }
+            validation = true;
+        }
+        return validation;
+    }
+
+    public static double calcMoy(int[] tabEntree){
+        double moyenne=0;
+
+        for (int i = 0; i < tabEntree.length; i++) {
+            moyenne += tabEntree[i];
+        }
+        moyenne = moyenne/tabEntree.length;
+
+        return moyenne;
+    }
+
+    public static void manipTab(){
+        int[] arrayTabSaisi;
+        int moy = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Veuillez choisir un nombre de valeurs à saisir: \n");
+        int  nb = scanner.nextInt();
+        arrayTabSaisi = new int[nb];
+
+        for (int i = 0; i < arrayTabSaisi.length; i++) {
+            System.out.println("Veuillez saisir le nombre " + (i+1) + " :\n");
+
+            //Met la valeur saisi dans le tableau
+            arrayTabSaisi[i] = scanner.nextInt();
+
+            //Sauvergarde des valeurs saisis pour calculer la moyenne
+            moy += arrayTabSaisi[i];
+
+            //compare les valeurs du tableau pour avoir le min et max
+            if(arrayTabSaisi[i]<min){
+                min = arrayTabSaisi[i];
+            }
+            if(arrayTabSaisi[i]>max){
+                max = arrayTabSaisi[i];
+            }
+        }
+        System.out.println("Tableau de nombres saisis : " + arrayTabSaisi +
+                            "\nMoyenne = " + (moy/arrayTabSaisi.length) +
+                            "\nMinimum = " + min +
+                            "\nMaximum = " + max);
+    }
+
+    public static int[] invert (int[] firstArray){
+
+        int[] newArray = new int[firstArray.length];
+        int lenght = newArray.length -1;
+
+        for (int i = firstArray.length - 1; i >= 0; i--) {
+            newArray[lenght - i] = firstArray[i];
+        }
+        return newArray;
+    }
+    public static int sum2DTab(int[][] array2D){
+        int result = 0;
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; j < array2D[i].length; j++) {
+                result += array2D[i][j];
+
+            }
+
+        }
+        return result;
+    }
+
+    public static int calcDiagonale (int [][] arrays2D, int diagonalType){
+        int rightLeftResult = 0;
+        int InverseResult =0;
+
+
+
+        for (int i = 0; i < arrays2D.length; i++) {
+            rightLeftResult += arrays2D[i][i];
+            InverseResult += arrays2D[arrays2D.length - 1 - i][i];
+        }
+        if (diagonalType == 1){
+            return rightLeftResult;
+        }else{
+            return InverseResult;
+        }
+
+
+    }
+
+    public static void drawTriangle(int niveau){
+        int i =1;
+        while (i <= niveau *2){
+            String triangle = "*";
+            if (i<niveau){
+            System.out.println(triangle.repeat(i));
+            } else if (i>=niveau) {
+
+            System.out.println(triangle.repeat(2*niveau-i));
+            }
+
+            i++;
+        }
+    }
+
+    public static void plusOuMoins(){
+        boolean nbFind = false;
+        Random r = new Random();
+        int n = r.nextInt(100);
+        int vie = 5;
+
+        System.out.println("Devinez le nombre situé entre 1 et 100. Vous avez " + vie + " vie");
+
+
+        while(vie>0 && !nbFind) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ecrivez un nombre: ");
+            int nbGuess = scanner.nextInt();
+
+            if (nbGuess == n){
+                System.out.println("vous avez trouvé, le nombre est: " + n);
+                nbFind = true;
+            }else if(nbGuess> n){
+                vie--;
+                System.out.println("le nombre à trouver est plus petit recommencer il vous reste "+ vie + " vie.");
+            }else{
+                vie--;
+                System.out.println("le nombre à trouver est plus grand recommencer il vous reste "+ vie + " vie.");
+            }
+
+        }
+    }
+
+    public static int voyelle(String str){
+        str = str.toLowerCase();
+
+        int compteur=0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' ||
+                    str.charAt(i) == 'e' ||
+                    str.charAt(i) == 'i' ||
+                    str.charAt(i) == 'o' ||
+                    str.charAt(i) == 'u' ||
+                    str.charAt(i) == 'y'){
+
+                compteur ++;
+
+            }
+        }
+
+        System.out.println("Il y a " + compteur + " voyelles au total dans le mot: " + str);
+        return compteur;
+    }
+
+    public static int countMaj(String str){
+        int compteur=0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i))){
+
+                compteur ++;
+
+            }
+        }
+
+        System.out.println("Il y a " + compteur + " Majuscule au total dans le String: " + str);
+        return compteur;
+    }
+
+    public static String minToMaj(String str){
+        str = str.toUpperCase();
+
+        return str;
+    }
+
+    public static String reverseString(String str){
+        String reverseWord = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            reverseWord += str.charAt(i);
+        }
+        return reverseWord;
+    }
+
+    public static String camelCase(String str){
+        String strInCamelCase ="";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isWhitespace(str.charAt(i))){
+                str.substring()
+            }
+        }
+    }
+
+}
---
+++ src/test/java/fr/afpa/ApplicationTest.java
@@ -0,0 +1,188 @@
+package fr.afpa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testIsAdultFunction() {
+
+       Application.isAdult(2002);
+
+
+    }
+
+    @Test
+    void testFindMaxMinFunction() {
+
+        Application.findMaxMin(5, 15, 52);
+
+
+    }
+
+    @Test
+    void testVoyelleOuConsonneFunction() {
+
+        Application.voyelleOuConsonne('b');
+
+
+    }
+
+    @Test
+    void testLunchPriceFunction() {
+        Application.lunchPrice();
+    }
+
+    @Test
+    void testSubYoutubeForFunction() {
+        int resultFor = Application.subYoutubeFor(2500, 24, 0.05);
+        assertEquals(8043, resultFor);
+
+        int resultWhile = Application.subYoutubeWhile(2500, 24, 0.05);
+        assertEquals( 8043, resultWhile);
+    }
+
+    @Test
+    void testSommeEntierFunction(){
+        int result = Application.sommeEntier(5);
+        assertEquals(15, result);
+    }
+
+    @Test
+    void testFizzBuzzFunction(){
+        Application.fizzBuzz(15);
+    }
+
+    @Test
+    void testCalcFactoFunction(){
+        int result = Application.calcFacto(5);
+        assertEquals(120, result);
+    }
+
+    @Test
+    public void testAfficherTabFunction(){
+        Application.afficherTab();
+
+    }
+
+
+
+/*-------------------exercice 5.2------------------*/
+    @Test
+    void testRechercheSeqForFunction(){
+        int result = Application.rechercheSeqFor(12);
+        assertEquals(12, result);
+    }
+
+    @Test
+    void testRechercheSeqWhileFunction(){
+        int result = Application.rechercheSeqWhile(3);
+        assertEquals(3, result);
+    }
+
+    @Test
+    void testRechercheSeqDoWhileFunction(){
+        int result = Application.rechercheSeqDoWhile(25);
+        assertEquals(25, result);
+    }
+
+    /*-------------------exercice 5.3------------------*/
+
+    @Test
+    void testSumBetweenFunction(){
+        int result = Application.sumBetween(2,5);
+        assertEquals(22, result);
+    }
+
+    /*-------------------exercice 5.3------------------*/
+
+    @Test
+    void testMinMaxTabFunction(){
+        int[] result = Application.minMaxTab(new int[]{5, 25, 3, 1, 52});
+        assertEquals(52, result[1]);
+        assertEquals(1,result[0]);
+    }
+
+    /*-------------------exercice 5.4------------------*/
+
+    @Test
+    void testSwapFunction(){
+        boolean result = Application.swap(1,3);
+        assertEquals(true,result);
+    }
+
+    /*-------------------exercice 5.5------------------*/
+
+    @Test
+    void testCalcMoyFunction(){
+        double result = Application.calcMoy(new int[]{15, 16, 13, 15, 14});
+        assertEquals(14.6, result);
+    }
+
+    @Test
+    void testInvertFunction(){
+        int[] result =Application.invert(new int[]{5, 25, 3, 1, 52});
+        assertArrayEquals(new int[]{52, 1, 3, 25, 5} , result);
+    }
+
+    @Test
+    void testSum2DTabFunction(){
+        int result = Application.sum2DTab(new int[][] {
+                {4, 6, 5},
+                {7, 11, 34},
+                {28, 6, 2},
+                {1, 2, 3},
+                {4, 3, 10}
+        });
+        assertEquals(126, result);
+    }
+
+    @Test
+    void testCalcDiagonaleFunction(){
+        int result = Application.calcDiagonale(new int[][] {
+                {4, 6, 5, 2},
+                {7, 11, 34, 1},
+                {28, 6, 2, 15},
+                {1, 2, 3, 19}
+        }, 0);
+
+        assertEquals(43, result);
+    }
+
+    @Test
+    void testvoyelleFunction (){
+        int result = Application.voyelle("Bonjour");
+        assertEquals(3, result);
+    }
+
+    @Test
+    void testCountMajFunction (){
+        int result = Application.countMaj("Hello World");
+        assertEquals(2, result);
+    }
+
+    @Test
+    void testMinToMajFunction (){
+        String result = Application.minToMaj("Hello World");
+        assertEquals("HELLO WORLD", result);
+    }
+
+    @Test
+    void testReverseStringFunction (){
+        String result = Application.reverseString("La programmation c'est sympa.");
+        assertEquals(".apmys tse'c noitammargorp aL", result);
+    }
+
+
+
+
+}
